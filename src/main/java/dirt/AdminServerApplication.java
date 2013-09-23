215e80dd8c3a470e60df7d67ab928d88156004b5
Dave Syer <dsyer@gopivotal.com>
2013-09-23 13:06:51 +0100

    Externalize parent context config

--- src/main/java/dirt/AdminServerApplication.java
+++ src/main/java/dirt/AdminServerApplication.java
@@ -2,29 +2,20 @@ package dirt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.xd.dirt.container.XDContainer;
 import org.springframework.xd.dirt.rest.RestConfiguration;
 
 @Configuration
 @EnableAutoConfiguration
+@ImportResource("classpath:" + XDContainer.XD_INTERNAL_CONFIG_ROOT + "admin-server.xml")
+@Import(RestConfiguration.class)
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication("classpath:"
-				+ XDContainer.XD_INTERNAL_CONFIG_ROOT + "xd-global-beans.xml");
-		application.setWebEnvironment(false);
-		SpringApplication child = new SpringApplication(AdminServerApplication.class,
-				RestConfiguration.class, "classpath:"
-						+ XDContainer.XD_INTERNAL_CONFIG_ROOT + "admin-server.xml");
-		GenericApplicationContext parent = (GenericApplicationContext) application
-				.run(args);
-		AnnotationConfigEmbeddedWebApplicationContext context = new AnnotationConfigEmbeddedWebApplicationContext();
-		context.setParent(parent);
-		child.setApplicationContext(context);
-		child.run(args);
+		SpringApplication.run(AdminServerApplication.class, args);
 	}
 
 }
