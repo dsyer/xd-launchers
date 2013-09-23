8e444d4166984dbce9725ff68ab4a5f0c5763c72
Dave Syer <dsyer@gopivotal.com>
2013-09-23 11:22:20 +0100

    Prefer SpringApplication constructor

--- src/main/java/dirt/AdminServerApplication.java
+++ src/main/java/dirt/AdminServerApplication.java
@@ -4,24 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.xd.dirt.container.XDContainer;
 import org.springframework.xd.dirt.rest.RestConfiguration;
 
 @Configuration
 @EnableAutoConfiguration
-@ImportResource("classpath:" + XDContainer.XD_INTERNAL_CONFIG_ROOT + "admin-server.xml")
-@Import(RestConfiguration.class)
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication("classpath:" + XDContainer.XD_INTERNAL_CONFIG_ROOT
-				+ "xd-global-beans.xml");
+		SpringApplication application = new SpringApplication("classpath:"
+				+ XDContainer.XD_INTERNAL_CONFIG_ROOT + "xd-global-beans.xml");
 		application.setWebEnvironment(false);
-		SpringApplication child = new SpringApplication(AdminServerApplication.class);
-		GenericApplicationContext parent = (GenericApplicationContext) application.run(args);
+		SpringApplication child = new SpringApplication(AdminServerApplication.class,
+				RestConfiguration.class, "classpath:"
+						+ XDContainer.XD_INTERNAL_CONFIG_ROOT + "admin-server.xml");
+		GenericApplicationContext parent = (GenericApplicationContext) application
+				.run(args);
 		AnnotationConfigEmbeddedWebApplicationContext context = new AnnotationConfigEmbeddedWebApplicationContext();
 		context.setParent(parent);
 		child.setApplicationContext(context);
